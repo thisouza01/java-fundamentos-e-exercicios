0ca6a2c0e51bffe89f2dc888d4b2c6da57e8e177
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-06-24 13:53:47 -0300

    adicionando outros exemplos

--- Exibe-string.java
+++ Exibe-string.java
@@ -11,3 +11,30 @@ public class Solution {
         System.out.println("Eu acho legal ser programador");
     }
 }
+
+#3
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Ellie é inteligente");
+        System.out.println("Ellie é inteligente");
+        System.out.println("Ellie é inteligente");
+        System.out.println("Ellie é inteligente");
+        System.out.println("Ellie é inteligente");
+    }
+}
+
+#4
+public class Solution {
+    public static void main(String[] args) {
+        int idadeNascimento = 3126 - 8;
+        System.out.println(idadeNascimento);
+    }
+}
+
+#5
+public class Solution {
+    public static void main(String[] args) {
+        String nome, cidade;
+        int idade;
+    }
+}
