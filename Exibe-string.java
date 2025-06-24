0425469581bb77bf9dcad9c41558ee9799195dad
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-06-24 13:40:58 -0300

    adicionando outra string

--- Exibe-string.java
+++ Exibe-string.java
@@ -1,5 +1,13 @@
+#1
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Amigo é muito inteligente");
+    }
+}
+
+#2
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Eu acho legal ser programador");
     }
 }
